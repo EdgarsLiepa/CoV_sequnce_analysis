e9f359edb63c0489507e85b01fdae37e4cdb02f6
edgars leipa <edgars.liepa@biomed.lu.lv>
2022-05-24 09:39:22 +0000

    added description and input control

--- parse.py
+++ parse.py
@@ -1,20 +1,37 @@
+# Author: Edgars Liepa
+# Date: 6.5.22 
+# Descripton: Create a list of amino acid substitutions from multiple sequences 
+# in single fasta file.
+# Programm firstly creates pairvise allignment to reference sequnce 
+# and then counts changed positions
+# 
+# Dependencies: BioPython   
+
+
 from Bio import SeqIO
 from datetime import datetime
+import csv
+import sys
+
+if 2 > len(sys.argv):
+    print("\nNo allignment file provided\nexample: python parse.py protein.fasta\n")
+    exit()
+
+print("Counting mutations for", sys.argv[1], "file")
+
 
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 print("Parse start time =", current_time)
 
-records = list(SeqIO.parse("../s_protein_NCBI_2.aln", "fasta"))
+records = list(SeqIO.parse(sys.argv[1], "fasta"))
 
 
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 print("Parse end Time =", current_time)
 
-# This is my reference
-print(records[0].id)  # first record
-print(records[0].description)
+print("Reference: ",records[0].description)
 
 mutationList = []
 haploTypeList = []
@@ -26,23 +43,39 @@ for index, letter in enumerate(records[0].seq):
 for i in range (1, len(records)):
     haploType = {}
     for index, letter in enumerate(records[i].seq):
-        if (letter not in mutationList[index]):       
+        if index >= len(mutationList): # sequence is larger than reference and aa sub. array
+            mutationList.append({letter: 1})             
+        elif (letter not in mutationList[index]): # aa sub. seen first time       
             mutationList[index][letter] = 1
             haploType[index] = [letter]
-        elif (letter != records[0].seq[index]):
-            mutationList[index][letter] += 1
-            haploType[index] = [letter]
+        else:   # aa sub is in the mutation list, increase aa counter
+            if (index < len(records[0].seq)): # index is in the reference range
+                if (letter != records[0].seq[index]):
+                    mutationList[index][letter] += 1
+                    haploType[index] = [letter]
+            else:
+                mutationList[index][letter] += 1
+                haploType[index] = [letter]
         haploTypeList.append(haploType)
-    print(haploType)  
+    # print(haploType)  
 
 # Print Results
-for index, position in enumerate(mutationList):
-    for aa in  position.keys():
-        if position[aa] != 0:
-            print("Postion: ", index+1, "aaSubst: ", aa, "Count: ", position[aa] )
+
+header = ['Postion', 'aaSubst', 'Count']
+
+with open('mutations.csv', 'w', encoding='UTF8', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
+
+    for index, position in enumerate(mutationList):
+        for aa in  position.keys():
+            if position[aa] != 0:
+                print("Postion: ", index+1, "aaSubst: ", aa, "Count: ", position[aa])
+                writer.writerow([index+1, aa, position[aa]])
 
 
-# print(mutationList)  
+
+print("AA substitition counts saved at ")  
 
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
